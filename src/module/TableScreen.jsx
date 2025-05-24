e1ae4e4b7c677710722ffc91be6591a45c205d84
Egle Vondrak <egle.vondrak@gmail.com>
2025-05-25 00:36:23 +0200

    Add table

--- src/module/TableScreen.jsx
+++ src/module/TableScreen.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import { useTranslation } from 'react-i18next';
+import axios from 'axios';
+import { DataTableCard2, DateTime } from 'asab_webui_components';
 
 export function TableScreen(props) {
-	const { t } = useTranslation();
+    const { t } = useTranslation();
 
-	return (
-		<Container className='h-100'>
-			{t('Training|Hello, there is nothing here yet!')}
-		</Container>
-	);
+    const columns = [
+        { title: 'Username', render: ({ row }) => <span>{row.username}</span> },
+        { title: 'Email', render: ({ row }) => <span>{row.email}</span> },
+        { title: 'Created', render: ({ row }) => <DateTime value={row.created} /> },
+        { title: 'Last Sign In', render: ({ row }) => <DateTime value={row.last_sign_in} /> },
+        { title: 'Address', render: ({ row }) => <span>{row.address}</span> },
+    ];
+
+    const loader = async ({ params }) => {
+        const response = await axios.get('https://devtest.teskalabs.com/data', { params: params });
+        const rows = response.data.data;
+        const count = response.data.count;
+        return { count, rows };
+    };
+
+    return (
+        <Container className='h-100'>
+            <DataTableCard2 columns={columns} loader={loader} header={<h1>Table</h1>} />
+        </Container>
+    );
 }
